8cc8ba5096b0b89c73a9560e701f9eaadc09beaf
Guoxiaoyong <sionguo@126.com>
2024-06-18 13:55:28 +0800

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'prettier',
   ],
@@ -14,5 +17,16 @@ module.exports = {
     'prettier/prettier': 'error',
     '@typescript-eslint/no-explicit-any': ['off'],
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
