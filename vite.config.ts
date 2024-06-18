0ccb6eb72095cf7977b4e62c051d3a32300d8ea7
Guoxiaoyong <sionguo@126.com>
2024-06-18 11:45:38 +0800

    modify

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import MillionLint from '@million/lint';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [MillionLint.vite(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
